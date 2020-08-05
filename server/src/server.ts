19e31c85c8793bc05240cf15a483cc6e32bd9a33
BrunoShimura <bruno_shimura@hotmail.com>
2020-08-05 11:08:59 -0300

    criando-tabela-usuarios

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,8 +2,10 @@ import express from 'express';
 
 const app = express();
 
-app.get('/users' , (request,response) => {
-  return response.send('hello world');
+app.use(express.json());
+
+app.get('/' , (request,response) => {
+  return response.json({message:'hello world'});  
 });
 
 // localhost:3333
