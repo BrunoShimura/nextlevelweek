250b2e8e97c992a1f9965d4c1ca4cbb7ef9a57b2
BrunoShimura <bruno_shimura@hotmail.com>
2020-08-06 10:54:58 -0300

    controller-insert

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,10 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
-
-app.get('/' , (request,response) => {
-  return response.json({message:'hello world'});  
-});
+app.use(routes);
 
 // localhost:3333
 app.listen(3333);
