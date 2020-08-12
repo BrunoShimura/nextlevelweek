557e35ff220bdc6a9facfb5335f31cdf0797b1b1
BrunoShimura <bruno_shimura@hotmail.com>
2020-08-12 11:26:23 -0300

    web-finalizado

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # NextLevelWeek
+
+NLW 🚀 é uma experiência online com muito conteúdo prático, desafios e hacks que ajuda o dev (Dev é a abreviação de developer, uma palavra que não tem gênero. Ou seja, dev pode ser tanto um programador quanto uma programadora 👨‍🚀👩‍🚀),  a avançar para o próximo nível. 
+
+web
+------------------
+![](https://gitlab.com/BrunoShimura/NextLevelWeek/-/wikis/uploads/c6fe13110daf989deaef032bf8ac2203/Screenshot_20200812_111927.png)
--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -3,31 +3,49 @@ import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
+import api from '../../services/api';
 
-function TeacherItem(){
+export interface Teacher {
+  id:number;
+  avatar: string;
+  bio: string;
+  cost: number;
+  name: string;
+  subject: string;
+  whatsapp: string;
+}
+
+interface TeacherItemProps{
+  teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({teacher}) => {
+
+  function createNewConnection(){
+    api.post('connections',{
+      user_id: teacher.id,
+    })
+  }
+
   return (
     <article className="teacher-item">
           <header> 
-            <img src="https://avatars2.githubusercontent.com/u/53915449?s=460&u=ac744d00394ddaa9cdca11aafc4e0e949fa2d730&v=4" alt="Bruno Shimura"/>
+            <img src={teacher.avatar} alt={teacher.name}/>
             <div>
-              <strong>Bruno Anthony Shimura</strong>
-              <span>Educação Física</span>
+              <strong>{teacher.name}</strong>
+              <span>{teacher.subject}</span>
             </div>
           </header>
-            <p>
-            Instrutor de Educação Física para iniciantes, minha missão de vida é levar saúde e contribuir para o crescimento de quem se interessar. 
-            <br/><br/>
-            Comecei a minha jornada profissional em 2001, quando meu pai me deu dois alteres de 32kg com a seguinte condição: "Aprenda a fazer dinheiro com isso!"
-            </p>
+            <p>{teacher.bio}</p>
             <footer>
               <p>
                 Preço/hora
-                <strong>R$ 100,00</strong>
+                <strong>R$ {teacher.cost}</strong>
               </p>
-              <button type="button">
+              <a target="_blank" onClick={createNewConnection} href={`https://wa.me/${teacher.whatsapp}`}>
                 <img src={whatsappIcon} alt="Whatssapp"/>
                 Entrar em contato
-              </button>
+              </a>
             </footer>
         </article>
   );
